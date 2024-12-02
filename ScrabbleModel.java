839e2dd67f83a5ce7a063991dc305edc2be3c042
riddlework <82472185+riddlework@users.noreply.github.com>
2024-12-02 14:16:23 -0700

    initialized model

--- ScrabbleModel.java
+++ ScrabbleModel.java
@@ -1,2 +1,21 @@
 public class ScrabbleModel {
+    Player p1;
+    Player p2;
+    Board board;
+    DrawPile drawPile;
+
+    public ScrabbleModel(Player p1, Player p2, Board board, DrawPile drawPile) {
+        this.p1 = p1;
+        this.p2 = p2;
+        this.board = board;
+        this.drawPile = drawPile;
+    }
+
+    // methods:
+        // count double/triple letter values when playing hand
+        // replace used tiles in hand with random ones
+        // swap tiles as turn
+
+
+
 }
