07a5b89cdf16b1183eac2ec5f8964ba2496603a3
benyurek <149546012+benyurek@users.noreply.github.com>
2024-12-04 15:19:35 -0700

    Add files via upload

---
+++ TileTests.java
@@ -0,0 +1,32 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TileTests {
+
+	Tile exampleTile;
+	Tile exampleTile2;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		exampleTile = new Tile('Q', 123);
+		exampleTile2 = new Tile();
+	}
+
+	
+	// tests letter getter
+	@Test
+	void testGetLetter() {
+		assertEquals(exampleTile.getLetter(), 'Q');
+		assertEquals(exampleTile2.getLetter(), '_');
+	}
+	
+	// tests points getter
+	@Test
+	void testGetPoints() {
+		assertEquals(exampleTile.getPoints(), 123);
+		assertEquals(exampleTile2.getPoints(), -999);
+	}
+
+}
